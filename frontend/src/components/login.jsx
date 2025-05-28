52adc5fda1cc8b43cfbb659479992feb624a0f47
Adnaniqbal30 <adnan30iqbal@gmail.com>
2025-05-28 22:24:53 +0530

    Update login component and add netlify.toml

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -12,10 +12,14 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('/api/auth/login', {
+      const response = await axios.post('https://zidio-excel-analytics.onrender.com/api/auth/login', {
         username,
         password,
+  },    {
+        withCredentials: true // if you want to send cookies, optional here
       });
+
+    
 
       // Store the JWT token in localStorage or cookie
       localStorage.setItem('token', response.data.token);
